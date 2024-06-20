corrected mistakes in ub calculation

src/tavi/instrument/tas.py
@@ -72,23 +72,29 @@ class TAS(object):
         """Calculate UB matrix from two peaks
 
         Args:
-            peaks (list)
-            angles (list)
+            peaks (list): lists of two tuples, [(h1,k1,l1), (h2,k2,l2)]
+            angles (list): lists of goniometer angles
             ki (float): incident neutron wavelength, in inv Ang
             kf (float): final neutron wavelength, in inv Ang
+
+        Returns:
+            u_mat: 3 by 3 unitary matrix
+            in_plane_ref: vector of peak1 in q_sample frame (zero goniometer angles)
+            plane_normal: normal vector of the scattering plane in q_sample frame,
+                            always pointing up (positive y-axis)
         """
 
         b_mat = self.sample.b_mat()
         peak1, peak2 = peaks
-        q_hkl1 = b_mat @ peak1 * 2 * np.pi
-        q_hkl2 = b_mat @ peak2 * 2 * np.pi
-        q_hkl3 = np.cross(q_hkl1, q_hkl2)
-        q_hkl_2p = np.cross(q_hkl3, q_hkl1)
+        q_hkl1 = b_mat @ peak1
+        q_hkl2p = b_mat @ peak2
+        q_hkl3 = np.cross(q_hkl1, q_hkl2p)
+        q_hkl_2 = np.cross(q_hkl3, q_hkl1)
 
         q_hkl_mat = np.array(
             [
                 q_hkl1 / np.linalg.norm(q_hkl1),
-                q_hkl_2p / np.linalg.norm(q_hkl_2p),
+                q_hkl_2 / np.linalg.norm(q_hkl_2),
                 q_hkl3 / np.linalg.norm(q_hkl3),
             ]
         ).T
@@ -101,25 +107,27 @@ class TAS(object):
         q_lab1 = TAS.q_lab(two_theta1, ki=ki, kf=kf)
         q_lab2 = TAS.q_lab(two_theta2, ki=ki, kf=kf)
 
-        q_sample1 = self.goniometer.r_mat_inv(angles1) @ q_lab1
-        q_sample2p = self.goniometer.r_mat_inv(angles2) @ q_lab2
+        # Goniometer angles all zeros in q_sample frame
+        q_sample1 = self.goniometer.r_mat_inv(angles1[1:]) @ q_lab1
+        q_sample2p = self.goniometer.r_mat_inv(angles2[1:]) @ q_lab2
         q_sample3 = np.cross(q_sample1, q_sample2p)
         q_sample2 = np.cross(q_sample3, q_sample1)
 
-        in_plane_ref = q_sample1 / np.linalg.norm(q_sample1)
-        plane_normal = q_sample3 / np.linalg.norm(q_sample3)
-        if plane_normal[1] < 0:  # always along +y
-            plane_normal = -plane_normal
-
         q_sample_mat = np.array(
             [
-                in_plane_ref,
+                q_sample1 / np.linalg.norm(q_sample1),
                 q_sample2 / np.linalg.norm(q_sample2),
-                plane_normal,
+                q_sample3 / np.linalg.norm(q_sample3),
             ]
         ).T
 
         u_mat = q_sample_mat @ np.linalg.inv(q_hkl_mat)
+
+        in_plane_ref = q_sample_mat[:, 0]
+        if q_sample_mat[1, 2] < 0:
+            plane_normal = -q_sample_mat[:, 2]
+        else:
+            plane_normal = q_sample_mat[:, 2]
 
         return u_mat, in_plane_ref, plane_normal
 
@@ -166,8 +174,36 @@ class TAS(object):
             print("I don't even know what you're doing.")
 
         self.sample.ub_matrix = ub_matrix
-        self.sample.inv_ub_matrix = np.linalg.inv(ub_matrix)
+        inv_ub_matrix = np.linalg.inv(ub_matrix)
+        self.sample.inv_ub_matrix = inv_ub_matrix
+
+        self.sample.u = inv_ub_matrix @ np.array([0, 0, 1])
+        self.sample.v = inv_ub_matrix @ np.array([1, 0, 0])
+
         # print(np.round(ub_matrix, 6))
+
+    def find_two_theta(self, peak, ei=13.5, ef=None):
+        """calculate two theta angle of a given peak
+
+        Args:
+            peak (tuple): (h, k, l) of a peak
+            ei (float): incident neutron energy, in meV
+            ef (float): final neutron energy, in meV
+        Return
+            two_theta: in degrees
+        """
+
+        hkl = np.array(peak)
+        ki = np.sqrt(ei / ksq2eng)
+        if ef is None:
+            kf = ki
+        else:
+            kf = np.sqrt(ef / ksq2eng)
+
+        b_mat = self.sample.b_mat()
+        q_sq = 4 * np.pi**2 * hkl.T @ b_mat.T @ b_mat @ hkl
+        two_theta = np.arccos((ki**2 + kf**2 - q_sq) / (2 * ki * kf)) * self.goniometer.sense
+        return two_theta * rad2deg
 
     def find_angles(self, peak, ei=13.5, ef=None):
         """calculate motor positions for a given peak if UB matrix has been determined
@@ -176,9 +212,6 @@ class TAS(object):
             peak (tuple): (h, k, l) of a peak
             ei (float): incident neutron energy, in meV
             ef (float): final neutron energy, in meV
-
-        Note:
-            two_theta = 2 * acrsin(|q|/(2k))
 
         """
         hkl = np.array(peak)
@@ -200,6 +233,7 @@ class TAS(object):
         eps = 1e-8  # zero
         plane_normal = self.sample.plane_normal
         in_plane_ref = self.sample.in_plane_ref
+
         # Minimal tilts
         if np.dot(t1, plane_normal) < eps:  # t1 in plane
             t3 = plane_normal
@@ -214,10 +248,6 @@ class TAS(object):
                 t3 = np.cross(t1, t2p)
                 t2 = np.cross(t3, t1)
 
-        # t2p = np.array([t1[2], 0, -t1[0]])
-        # t3 = np.cross(t1, t2p)
-        # t2 = np.cross(t3, t1)
-
         t_mat = np.array(
             [t1, t2 / np.linalg.norm(t2), t3 / np.linalg.norm(t3)],
         ).T
@@ -231,8 +261,7 @@ class TAS(object):
         q_lab_mat = np.array([q_lab1, q_lab2, q_lab3]).T
         r_mat = q_lab_mat @ t_mat_inv
 
-        angles = self.goniometer.angles_from_r_mat(r_mat)
-        angles = (two_theta * rad2deg,) + angles
+        angles = np.round((two_theta * rad2deg,) + self.goniometer.angles_from_r_mat(r_mat), 5)
 
         print(angles)
         return angles
@@ -257,7 +286,7 @@ if __name__ == "__main__":
     ]
 
     takin_instru.find_ub(peaks=peak_list, angles=angles_list, ei=13.500172, ef=13.505137)
-    angles = takin_instru.find_angles(peak=(0, 0, 1), ei=13.50172, ef=13.505137)
+    angles = takin_instru.find_angles(peak=(0, 2, 2), ei=13.50172, ef=13.505137)
 
     # describe and plot ellipses
     # ellipses = reso.calc_ellipses(res["reso"], verbose)

src/tavi/instrument/tas_cmponents.py
@@ -204,7 +204,7 @@ class Goniometer(object):
         "rotation matrix"
         # TODO
         if self.type == "TAS":  # Y-mZ-X for HB1A
-            _, omega, sgl, sgu = angles  # s2, s1, sgl, sgu
+            omega, sgl, sgu = angles  # s2, s1, sgl, sgu
             r_mat = rot_y(omega) @ rot_z(-sgl) @ rot_x(sgu)
 
         elif self.type == "4C":
@@ -215,7 +215,8 @@ class Goniometer(object):
 
     def r_mat_inv(self, angles):
         """inverse of rotation matrix"""
-        return np.linalg.inv(self.r_mat(angles))
+        # return np.linalg.inv(self.r_mat(angles))
+        return self.r_mat(angles).T
 
     def angles_from_r_mat(self, r_mat):
         """Calculate goniometer angles from the R matrix"""