55128893695d04078917be8b4a1314933f6cbd5b
Bing Li <lib1@ornl.gov>
2024-07-29 15:46:01 -0400

    updated ub calculation in the Xtal class

--- src/tavi/sample/xtal.py
+++ src/tavi/sample/xtal.py
@@ -15,7 +15,7 @@ class Xtal(Sample):
 
     """
 
-    def __init__(self, lattice_params):
+    def __init__(self, lattice_params=(1, 1, 1, 90, 90, 90)):
         super().__init__(lattice_params)
         self.type = "xtal"
 
@@ -28,9 +28,42 @@ class Xtal(Sample):
 
         self.i_star, self.j_star, self.k_star = self.reciprocal_basis()
 
+    @staticmethod
+    def ub_matrix_to_uv(ub_matrix):
+        """ "Calculate u and v vector from UB matrix"""
+        inv_ub_matrix = np.linalg.inv(ub_matrix)
+        u = inv_ub_matrix @ np.array([0, 0, 1])
+        v = inv_ub_matrix @ np.array([1, 0, 0])
+        return (u, v)
 
-if __name__ == "__main__":
-    xtal = Xtal(lattice_params=(3.574942, 3.574942, 5.663212, 90, 90, 120))
+    @staticmethod
+    def ub_matrix_to_lattice_params(ub_matrix):
+        """Calculate lattice parameters from UB matrix"""
+        g_mat = np.linalg.inv(ub_matrix.T @ ub_matrix)
+        a = np.sqrt(g_mat[0, 0])
+        b = np.sqrt(g_mat[1, 1])
+        c = np.sqrt(g_mat[2, 2])
+        alpha = np.arccos((g_mat[1, 2] + g_mat[2, 1]) / (2 * b * c)) * rad2deg
+        beta = np.arccos((g_mat[0, 2] + g_mat[2, 0]) / (2 * a * c)) * rad2deg
+        gamma = np.arccos((g_mat[0, 1] + g_mat[1, 0]) / (2 * a * b)) * rad2deg
+        return (a, b, c, alpha, beta, gamma)
 
-    print(xtal.b_mat() / 2 / np.pi)
-    print(xtal.b_mat() @ np.array([0, 1, 0]) / 2 / np.pi)
+    def uv_to_ub_matrix(self, u, v):
+        """Calculate UB matrix from u and v vector, and lattice parameters"""
+
+        b_mat = self.b_mat()
+        t1 = b_mat @ u
+        t2_prime = b_mat @ v
+        t3 = np.cross(t1, t2_prime)
+        t2 = np.cross(t3, t1)
+        t_mat = np.array(
+            [
+                t1 / np.linalg.norm(t1),
+                t2 / np.linalg.norm(t2),
+                t3 / np.linalg.norm(t3),
+            ]
+        ).T
+        q_mat = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]]).T
+        ub_matrix = q_mat @ np.linalg.inv(t_mat) @ b_mat
+
+        return ub_matrix
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,34 +1,34 @@
 import numpy as np
 from tavi.sample.sample import Sample
+from tavi.sample.xtal import Xtal
 
 
-def test_b_matrix(a, b, c, alpha, beta, gamma, b_matrix):
+def test_b_matrix(lattice_params, b_matrix):
 
-    sample = Sample(lattice_params=(a, b, c, alpha, beta, gamma))
+    sample = Sample(lattice_params=lattice_params)
     print(sample.b_mat())
     print(np.allclose(sample.b_mat(), b_matrix, atol=1e-4))
 
 
-def test_ub_matrix_to_uv(a, b, c, alpha, beta, gamma, ub_matrix):
+def test_ub_matrix_to_uv(lattice_params, ub_matrix):
 
-    sample = Sample(lattice_params=(a, b, c, alpha, beta, gamma))
-    (u, v) = sample.ub_matrix_to_uv(ub_matrix)
+    xtal = Xtal(lattice_params=lattice_params)
+    (u, v) = xtal.ub_matrix_to_uv(ub_matrix)
     print(f"u={u}")
     print(f"v={v}")
 
 
 def test_ub_matrix_to_lattice_params(ub_matrix):
-    sample = Sample()
+    sample = Xtal()
     (a, b, c, alpha, beta, gamma) = sample.ub_matrix_to_lattice_params(ub_matrix)
     print(f"a={np.round(a,5)}, b={np.round(b,5)},c={np.round(c,5)}")
-    print(f"alpha={int(alpha)}, beta={int(beta)}, gamma={int(gamma)}")
+    print(f"alpha={np.round(alpha,3)}, beta={np.round(beta,3)}, gamma={np.round(gamma,3)}")
 
 
 def test_uv_to_ub_matrix(u, v, lattice_params):
-    sample = Sample(lattice_params=lattice_params)
-    u_matrix, b_matrix, ub_matrix = sample.uv_to_ub_matrix(u, v)
-    print(f"u matrix = {u_matrix}")
-    print(f"b matrix = {b_matrix}")
+    sample = Xtal(lattice_params=lattice_params)
+    ub_matrix = sample.uv_to_ub_matrix(u, v)
+
     print(f"ub matrix = {ub_matrix}")
 
 
@@ -40,15 +40,28 @@ if __name__ == "__main__":
     alpha = 90
     beta = 90
     gamma = 120
+
     ub_matrix = np.array(
-        [[0.053821, 0.107638, 0.166485], [0.272815, -0.013290, 0.002566], [0.164330, 0.304247, -0.058788]]
+        [
+            [0.053821, 0.107638, 0.166485],
+            [0.272815, -0.013290, 0.002566],
+            [0.164330, 0.304247, -0.058788],
+        ]
     )
-    b_matrix = np.array([[0.3230, 0.1615, 0.0000], [0.0000, 0.2797, -0.0000], [0.0000, 0.0000, 0.1766]])
+    b_matrix = np.array(
+        [
+            [0.3230, 0.1615, 0.0000],
+            [0.0000, 0.2797, -0.0000],
+            [0.0000, 0.0000, 0.1766],
+        ]
+    )
 
     u = [0.15623, 2.83819, -1.88465]
     v = [-0.00060, 1.03219, 5.33915]
 
-    # test_b_matrix(a, b, c, alpha, beta, gamma, b_matrix)
-    # test_ub_to_uv(a, b, c, alpha, beta, gamma, ub_matrix)
+    lattice_params = (a, b, c, alpha, beta, gamma)
+
+    # test_b_matrix(lattice_params, b_matrix)
+    # test_ub_matrix_to_uv(lattice_params, ub_matrix)
     # test_ub_matrix_to_lattice_params(ub_matrix)
-    test_uv_to_ub_matrix(u, v, (a, b, c, alpha, beta, gamma))
+    test_uv_to_ub_matrix(u, v, lattice_params)
