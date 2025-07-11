97eec49dfe04e45ddbeeee5ebc3564933e20e779
Bing Li <lib1@ornl.gov>
2025-07-11 16:05:37 -0400

    testing xarray

--- scripts/xarray_test.py
+++ scripts/xarray_test.py
@@ -1,27 +1,43 @@
-import numpy as np
 import xarray as xr
 
 # Example coordinates and detector data
-x = [1, 2, 3, 4, 5, 6, 7]
-detector = [10.5, 11.0, 9.8, 12.3, 13.1, 14.0, 15.2]
+# x = [1, 2, 3, 4, 5, 6, 7]
+# detector = [10.5, 11.0, 9.8, 12.3, 13.1, 14.0, 15.2]
 
-da = xr.DataArray(
-    data=detector,
-    dims="points",
-    coords={
-        "points": x,
-        "x": ("points", [x0 * 2 for x0 in x]),
-    },
-    name="detector",
-)
+# da = xr.DataArray(
+#     data=detector,
+#     dims="points",
+#     coords={
+#         "points": x,
+#         "x": ("points", [x0 * 2 for x0 in x]),
+#     },
+#     name="detector",
+# )
 
-print(da)
+# print(da)
 
 
-# Define bin edges
-bins = np.arange(0, 8, 2)  # [0, 2, 4, 6]
+# # Define bin edges
+# bins = np.arange(0, 8, 2)  # [0, 2, 4, 6]
 
-# Group by x bins
-binned = da.groupby_bins("points", bins=bins, right=False).mean()
+# # Group by x bins
+# binned = da.groupby_bins("points", bins=bins, right=False).mean()
 
-print(binned)
+# print(binned)
+
+
+# Create datasets
+ds_root = xr.Dataset({"a": ("x", [1, 2, 3])})
+ds_child = xr.Dataset({"b": ("x", [4, 5, 6])})
+
+# Create DataTree
+tree = xr.DataTree(ds_root, name="root")
+tree["child"] = xr.DataTree(ds_child, name="child")
+
+# Write to HDF5/NetCDF file
+tree.to_netcdf("tree_output.h5", engine="h5netcdf")
+
+# Read it back
+tree_loaded = xr.open_datatree("tree_output.h5", engine="h5netcdf")
+
+print(tree_loaded)
--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -108,3 +108,58 @@ def test_dataset_plotting(spice_data):
     ax.grid(alpha=0.6)
     plt.tight_layout()
     plt.show()
+
+
+def test_dataset_to_h5(spice_data):
+    ds = dict_to_xr_dataset(*spice_data[0])
+
+    root = xr.DataTree(name="root")
+    root["ds"] = xr.DataTree(ds, name="ds")
+    root.to_netcdf("saved_on_disk.h5", engine="h5netcdf")
+
+    # Read it back
+    root = xr.open_datatree("saved_on_disk.h5", engine="h5netcdf")
+    print(root)
+
+
+def test_dataset_renorm(spice_data):
+    ds = dict_to_xr_dataset(*spice_data[0])
+
+    signal = ds["signal"]
+    norm = ds.coords["mcu"]
+    signal_renormed = signal / norm
+
+    _, ax = plt.subplots()
+    ax.plot(ds.coords["e"], signal)
+    ax.plot(ds.coords["e"], signal_renormed * 30.0)
+    plt.show()
+
+
+def test_sc_xarray_conversion(spice_data):
+    xr_array = dict_to_xr_data_array(*spice_data[0])
+    assert xr_array.attrs["NX_class"] == "NXentry"
+    sc_da = sc.compat.xarray_compat.from_xarray(xr_array)
+    assert isinstance(sc_da, sc.DataArray)
+
+
+def test_xr_data_array_rebin(spice_data):
+    da = dict_to_xr_data_array(*spice_data[0])
+
+    bins = np.arange(0, 5, 0.05) - 0.05 / 2  # [0 1,2,3,4]
+    da_binned = da.groupby_bins("e", bins=bins, right=False).mean()
+
+    # Extract bin centers from Interval objects
+    intervals = da_binned.coords["e_bins"].values  # pandas.Interval objects
+    bin_centers = np.array([interval.left + (interval.right - interval.left) / 2 for interval in intervals])
+
+    da_binned = da_binned.assign_coords(e_bins=("e_bins", bin_centers))
+    da_binned = da_binned.rename({"e_bins": "e"})
+
+    (fig, ax0) = plt.subplots()
+
+    ax0.plot(da.coords["e"], da, "-o", label="original")
+    ax0.plot(da_binned.coords["e"], da_binned, "-o", label="binned")
+    ax0.legend()
+    ax0.grid(alpha=0.6)
+    plt.tight_layout()
+    plt.show()
