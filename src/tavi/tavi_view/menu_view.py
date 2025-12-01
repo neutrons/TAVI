932a3b1b96c42c4e2a82472f9ed3366cfc2c2cef
Kyle Ma <kyleqianlima@gmail.com>
2025-12-01 11:18:41 -0500

    docstrings for menubar

--- src/tavi/tavi_view/menu_view.py
+++ src/tavi/tavi_view/menu_view.py
@@ -2,7 +2,31 @@ from qtpy.QtWidgets import QAction, QApplication, QFileDialog, QMenuBar
 
 
 class MainMenuBar(QMenuBar):
+    """
+    Main application menu bar for TAVI, providing project and file-loading actions.
+
+    This menu bar defines the standard "File" menu for the TAVI GUI, including:
+    - New Project
+    - Load Project
+    - Load File(s)
+    - Load Folder
+    - Save Project
+    - Exit
+
+    Each menu action triggers a handler method that emits callbacks
+    (registered via `setup_callback_*` methods) which are called in presenters.
+    """
+
     def __init__(self, parent=None):
+        """
+        Initialize the main menu bar, create all file-related actions, and connect
+        them to internal handlers.
+
+        Parameters
+        ----------
+        parent : QWidget, optional
+            Parent widget, typically the main window.
+        """
         super().__init__(parent)
         self.load_folder_callback = None
 
@@ -31,35 +55,75 @@ class MainMenuBar(QMenuBar):
         self.exit_action.triggered.connect(self.handle_exit)
 
     # TODO Loading a new taviproject
-    def connect_new_project(self, callback):
+    def setup_callback_new_project(self, callback: None) -> None:
+        """
+        Register a callback that is invoked when a new project should be created.
+
+        Parameters
+        ----------
+        callback : Callable
+            Function to be called to initialize a new TAVI project.
+        """
         self.load_file_callback = callback
 
-    def new_project(self):
+    def new_project(self) -> None:
+        """
+        Create a new TAVI project.
+        Placeholder logic.
+        """
         print("TODO: creates a new taviproject")
 
-    def handle_new_project(self):
+    def handle_new_project(self) -> None:
+        """
+        Handler for the 'New Project' menu action.
+        Placeholder logic
+        """
         print("TODO: delete everything in taviproject")
 
     # TODO Loading a new project
-    def connect_load_project(self, callback):
+    def setup_callback_load_project(self, callback: None) -> None:
+        """
+        Register a callback invoked when a project should be loaded.
+
+        Parameters
+        ----------
+        callback : Callable
+            Function called when the 'Load Project' action is triggered.
+        """
         self.load_file_callback = callback
 
-    def load_project(self):
+    def load_project(self) -> None:
+        """
+        Load a TAVI project.
+        Placeholder logic.
+        """
         print("TODO: creates a new taviproject")
 
-    def handle_load_project(self):
+    def handle_load_project(self) -> None:
+        """
+        Handler for the 'Load Project' action.
+        Placeholder logic.
+        """
         print("TODO: delete everything in taviproject")
 
     # Loading a folder of data
-    def connect_load_folder(self, callback):
-        """Building callback connections for the load data - set by the presenter"""
+    def setup_callback_load_folder(self, callback) -> None:
+        """
+        Building callback connections for the load data - set by the presenter
+        """
         self.load_folder_callback = callback
 
-    def load_folder(self, folder):
-        """Pass loaded file through callback connections"""
+    def load_folder(self, folder) -> None:
+        """
+        Pass loaded file through callback connections
+        """
         self.load_folder_callback(folder)
 
-    def handle_load_folder(self):
+    def handle_load_folder(self) -> None:
+        """
+        Opens a system window and allow users to select a folder directory.
+        It executes the "load_folder" function.
+        """
         dlg = QFileDialog(self, "Select a folder")
         dlg.setFileMode(QFileDialog.Directory)
         dlg.setOption(QFileDialog.Option.ShowDirsOnly, False)
@@ -69,27 +133,62 @@ class MainMenuBar(QMenuBar):
             self.load_folder(folder)
 
     # TODO Loading a new file
-    def connect_load_file(self, callback):
+    def setup_callback_load_file(self, callback: None) -> None:
+        """
+        Register a callback invoked when files should be loaded.
+
+        Parameters
+        ----------
+        callback : Callable
+            Function that will receive a list of file paths selected by the user.
+        """
         self.load_file_callback = callback
 
-    def load_file(self, list_of_file):
+    def load_file(self, list_of_file) -> None:
+        """
+        Load a list of files into the current TAVI project.
+        Placeholder logic.
+        """
         print("TODO: Loading a list of files")
 
-    def handle_load_files(self):
+    def handle_load_files(self) -> None:
+        """
+        Handler for the 'Load File(s)' action.
+        Placeholder logic.
+        """
         print("TODO: get list of files and call self.load_file to load")
 
     # TODO Saving TAVI project
-    def connect_save(self, callback):
+    def setup_callback_save(self, callback: None) -> None:
+        """
+        Register a callback that saves the current TAVI project.
+
+        Parameters
+        ----------
+        callback : Callable
+            Function invoked to serialize and store the project.
+        """
         self.save_callback = callback
 
-    def save(self):
+    def save(self) -> None:
+        """
+        Save the current TAVI project.
+        Placeholder logic.
+        """
         print("TODO: save current taviproject")
 
-    def handle_save(self):
+    def handle_save(self) -> None:
+        """
+        Handler for the 'Save Project' action.
+        Placeholder logic.
+        """
         print("TODO: get taviproject and write to local disk")
 
     # Exit
-    def handle_exit(self):
+    def handle_exit(self) -> None:
+        """
+        Exit Tavi
+        """
         window = self.window()  # the top-level QMainWindow
         if window:
             window.close()
