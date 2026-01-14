5a2a6bb80913bb53569b6eaff938ac6917444a9a
Michael Walsh <walshmm@ornl.gov>
2026-01-14 14:28:52 -0500

    fix some pre-commit errors?

--- src/tavi/__main__.py
+++ src/tavi/__main__.py
@@ -1,25 +1,18 @@
 """Entry point."""
 
-import asyncio
-import os
+import argparse
 import sys
 
-from qtpy.QtWidgets import QApplication
-
 from tavi import __version__ as tavi_version
-from tavi.backend.model.interface.TaviProjectInterface import TaviProjectInterface
-from tavi.configuration import Configuration
 from tavi.frontend.main import start
-from tavi.frontend.presenter.main_presenter import MainPresenter
 
 
-def _print_text_splash():
+def _print_text_splash() -> None:
     # TODO
     pass
 
-def _createArgparser():
-    import argparse
 
+def _createArgparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="TAVI", description="Triple Axis data Visualization Toolkit (TAVI) ", epilog="https://tavi.readthedocs.io/"
     )
@@ -32,14 +25,14 @@ def _createArgparser():
     return parser
 
 
-
-def main(args=None):
+def main(args: list[str] = None) -> int:
+    """Setups up and runs the application."""
     parser = _createArgparser()
     options, _ = parser.parse_known_args(args)
 
     # show the ascii splash screen
     _print_text_splash()
-    
+
     return start(options)
 
 
