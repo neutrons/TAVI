10c3ea317752ceadcdc7675dc11fa8bbe4340c46
Michael Walsh <walshmm@ornl.gov>
2026-01-23 13:04:31 -0500

    remove prototype code

--- src/tavi/frontend/main.py
+++ src/tavi/frontend/main.py
@@ -1,91 +1,18 @@
+"""Main Qt entry point."""
+
 import signal
 import sys
-import threading
+from argparse import Namespace
 
 from qtpy.QtCore import QTimer
-from qtpy.QtWidgets import QApplication, QHBoxLayout, QPushButton, QVBoxLayout, QWidget
+from qtpy.QtWidgets import QApplication
 
-from tavi.backend.model.help_model import help_function
-from tavi.backend.model.interface.tavi_project_interface import TaviProjectProxy
 from tavi.backend.model.interface.TaviProjectInterface import TaviProjectInterface
-from tavi.backend.model.interface.template_model_interface import TemplateModelProxy
-from tavi.backend.model.tavi_project_model import TaviProject
-from tavi.backend.model.template_model import TemplateModel
 from tavi.configuration import Configuration
-from tavi.frontend.presenter.load_presenter import LoadPresenter
-from tavi.frontend.presenter.menu_presenter import MenuPresenter
-from tavi.frontend.presenter.metadata_presenter import MetaDataPresenter
-from tavi.frontend.presenter.template_presenter import TemplatePresenter
-from tavi.frontend.view.load_view import LoadView
-from tavi.frontend.view.menu_view import MainMenuBar
-from tavi.frontend.view.metadata_view import MetaDataView
-from tavi.frontend.view.template_view import TemplateView
-
-"""Main Qt window"""
+from tavi.frontend.presenter.main_presenter import MainPresenter
 
 
-class MainWindow(QWidget):
-    """Main widget."""
-
-    def __init__(self, parent=None) -> None:
-        """Constructor."""
-        super().__init__(parent)
-
-        print(f"main GUI running on {threading.current_thread().name}")
-        ### Create widgets here ###
-        # initialize view
-
-        #!!!!!!!!!!!!!!!!!!!!
-        # self.load_view = load_view
-        # self.load_view.setParent(self)
-        #!!!!!!!!!!!!!!!!!!!!
-
-        load_view = LoadView(self)
-        metadata_view = MetaDataView(self)
-        random_view = TemplateView(self)
-
-        # initialize model/Proxy
-        tavi_dummy_model = TaviProject()
-        tavi_dummy_model_proxy = TaviProjectProxy(tavi_dummy_model)
-
-        random_model = TemplateModel()
-        random_model_proxy = TemplateModelProxy(random_model)
-
-        # pass proxy to presenter
-        self.load_presenter = LoadPresenter(load_view, tavi_dummy_model_proxy)
-        self.metadata_presenter = MetaDataPresenter(metadata_view, tavi_dummy_model_proxy)
-        self.random_presenter = TemplatePresenter(random_view, random_model_proxy)
-
-        ### Set the layout
-        layout = QVBoxLayout()
-        menu_bar = MainMenuBar(self)
-        layout.setMenuBar(menu_bar)
-        self.menu_presenter = MenuPresenter(menu_bar, tavi_dummy_model_proxy)
-
-        layout.addWidget(load_view)
-        layout.addWidget(metadata_view)
-        layout.addWidget(random_view)
-        ### Create bottom interface here ###
-
-        # Help button
-        help_button = QPushButton("Help")
-        help_button.clicked.connect(self.handle_help)
-
-        # Set bottom interface layout
-        hor_layout = QHBoxLayout()
-        hor_layout.addWidget(help_button)
-        layout.addLayout(hor_layout)
-
-        self.setLayout(layout)
-
-        # register child widgets to make testing easier
-        self.load_view = load_view
-
-    def handle_help(self) -> None:
-        help_function(context="tavi_View")
-
-
-def _qapp():
+def _qapp() -> QApplication:
     if QApplication.instance():
         _app = QApplication.instance()
     else:
@@ -93,7 +20,8 @@ def _qapp():
     return _app
 
 
-def start(options=None):
+def start(options: Namespace = None) -> int:
+    """Start the UI."""
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     app = _qapp()
@@ -114,8 +42,8 @@ def start(options=None):
     print("Welcome to TAVI!  Happy visualizing!")
     try:
         dict_of_model = {"TaviProjectInterface": TaviProjectInterface()}
-        mainWindow = MainWindow()
-        mainWindow.show()
+        mainWindow = MainPresenter(dict_of_model)
+        mainWindow._view.show()
 
         if options.headcheck:
             SECONDS = 3  # arbitrarily chosen
@@ -123,6 +51,6 @@ def start(options=None):
             QTimer.singleShot(SECONDS * 1000, lambda: app.exit(0))
         return app.exec()
 
-    except Exception:
+    except Exception:  # noqa: BLE001
         # TODO: logger.exception("Uncaught Error bubbled up to main!")
         return -1
