cf414231317f57960e73594f26293ff6e7b3b84f
Michael Walsh <walshmm@ornl.gov>
2026-01-28 15:05:49 -0500

    migrate how we check version to the standard

--- src/tavi/__init__.py
+++ src/tavi/__init__.py
@@ -1,16 +1,6 @@
-"""
-Contains the entry point for the application
-"""
+"""Contain the entry point for the application."""
 
 try:
     from ._version import __version__  # noqa: F401
 except ImportError:
     __version__ = "unknown"
-
-
-def Tavi():  # noqa: N802
-    """Start Class"""
-
-    from .tavimain import Tavi as tavi  # noqa: E501, N813
-
-    return tavi()
