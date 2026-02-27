d5623fa6a269ba4a4697a2f19e3b0a6b2205ca22
Kyle Ma <kyleqianlima@gmail.com>
2026-02-27 15:15:01 -0500

    pre-commit fix

--- src/tavi/library/algorithms/ub_algorithms.py
+++ src/tavi/library/algorithms/ub_algorithms.py
@@ -2,15 +2,20 @@
 Calculate UB matrix and related quantities.
 
 Follows Andrei Savici's UB Matrix Formlism used in mantid at
+Calculate UB matrix and related quantities.
+
+Follows Andrei Savici's UB Matrix Formlism used in mantid at
 https://github.com/mantidproject/documents/blob/main/Design/UBMatriximplementationnotes.pdf.
 Equations listed in the comments refer to the document above.
 """
 
+
 import numpy as np
 
-from tavi.library.utilities import SE2K
+from tavi.library.utilities import SE2K, Peak
 
 
+def q_lab(ei: float, ef: float, theta: float, phi: float) -> np.ndarray:
 def q_lab(ei: float, ef: float, theta: float, phi: float) -> np.ndarray:
     """
     Return qlab matrix. Follow's mantid convention. Eq.8.
@@ -21,9 +26,25 @@ def q_lab(ei: float, ef: float, theta: float, phi: float) -> np.ndarray:
         theta: angle between scattered beam and z axis, indegree.
         phi: the angle between scattered beam's projection onto the (x,y) plane and x axis.
 
+
     """
     ki = SE2K(ei)
     kf = SE2K(ef)
+    return np.array(
+        [
+            -kf * np.sin(np.radians(theta)) * np.cos(np.radians(phi)),
+            -kf * np.sin(np.radians(theta)) * np.sin(np.radians(phi)),
+            ki - kf * np.cos(np.radians(theta)),
+        ]
+    )
+
+
+def ub_to_uv(ub_mat: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    If ubmatrix is given, return uv vector.
+
+    Given in TAS and mantid case, u is along [0, 0, 1], v is along [1, 0, 0].
+    """
     return np.array(
         [
             -kf * np.sin(np.radians(theta)) * np.cos(np.radians(phi)),
@@ -45,16 +66,28 @@ def ub_to_uv(ub_mat: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
 
 
 def b_mat(lattice_params: tuple[float, float, float, float, float, float]) -> np.ndarray:
+def b_mat(lattice_params: tuple[float, float, float, float, float, float]) -> np.ndarray:
     """Calculate b matrix from lattice parameters.Eq.52."""
     a, b, c, alpha, beta, gamma = lattice_params
     cos_alpha = np.cos(np.radians(alpha))
     sin_alpha = np.sin(np.radians(alpha))
 
+
     cos_beta = np.cos(np.radians(beta))
     sin_beta = np.sin(np.radians(beta))
 
+
     cos_gamma = np.cos(np.radians(gamma))
     sin_gamma = np.sin(np.radians(gamma))
+
+    Vabg = np.sqrt(1 - cos_alpha**2 - cos_beta**2 - cos_gamma**2 + 2 * cos_alpha * cos_beta * cos_gamma)
+
+    a_star = sin_alpha / (a * Vabg)
+    b_star = sin_beta / (b * Vabg)
+    c_star = sin_gamma / (c * Vabg)
+
+    cos_alpha_star = (cos_beta * cos_gamma - cos_alpha) / (sin_beta * sin_gamma)
+    cos_beta_star = (cos_gamma * cos_alpha - cos_beta) / (sin_gamma * sin_alpha)
 
     Vabg = np.sqrt(1 - cos_alpha**2 - cos_beta**2 - cos_gamma**2 + 2 * cos_alpha * cos_beta * cos_gamma)
 
@@ -68,8 +101,16 @@ def b_mat(lattice_params: tuple[float, float, float, float, float, float]) -> np
     cos_gamma_star = (cos_alpha * cos_beta - cos_gamma) / (sin_alpha * sin_beta)
     sin_gamma_star = np.sqrt(1 - cos_gamma_star**2)
 
+    cos_gamma_star = (cos_alpha * cos_beta - cos_gamma) / (sin_alpha * sin_beta)
+    sin_gamma_star = np.sqrt(1 - cos_gamma_star**2)
+
     B = np.array(
         [
+            [a_star, b_star * cos_gamma_star, c_star * cos_beta_star],
+            [0, b_star * sin_gamma_star, -c_star * sin_beta_star * cos_alpha],
+            [0, 0, 1.0 / c],
+        ]
+    )
             [a_star, b_star * cos_gamma_star, c_star * cos_beta_star],
             [0, b_star * sin_gamma_star, -c_star * sin_beta_star * cos_alpha],
             [0, 0, 1.0 / c],
@@ -78,6 +119,9 @@ def b_mat(lattice_params: tuple[float, float, float, float, float, float]) -> np
     return B
 
 
+def uv_to_ub(
+    u: np.ndarray, v: np.ndarray, lattice_params: tuple[float, float, float, float, float, float]
+) -> np.ndarray:
 def uv_to_ub(
     u: np.ndarray, v: np.ndarray, lattice_params: tuple[float, float, float, float, float, float]
 ) -> np.ndarray:
@@ -94,8 +138,61 @@ def uv_to_ub(
         ]
     ).T
     T_epsilon = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]]).T
+    T_c = np.array(
+        [
+            t1_c / np.linalg.norm(t1_c),
+            t2_c / np.linalg.norm(t2_c),
+            t3_c / np.linalg.norm(t3_c),
+        ]
+    ).T
+    T_epsilon = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]]).T
     u_mat = T_epsilon @ T_c.T
     return u_mat @ B
 
 
 # -----------Calculate UB Matrix----------------------------
+
+
+def find_u_from_two_peaks(
+    peaks: tuple[Peak, Peak], b_mat: np.ndarray, r_mat: np.ndarray, ei: float, ef: float
+) -> np.ndarray:
+    """
+    Calculate U matrix from two peaks.
+
+    Follow Eq.76-81 and Eq.83-88. We assume q_3 is perpendicular from the two peaks
+    """
+    peak1, peak2 = peaks
+    B = b_mat
+
+    t1_c = B @ peak1.hkl
+    t3_c = np.cross(B @ peak1.hkl, B @ peak2.hkl)
+    t2_c = np.cross(t3_c, t1_c)
+
+    T_c = np.array(
+        [
+            t1_c / np.linalg.norm(t1_c),
+            t2_c / np.linalg.norm(t2_c),
+            t3_c / np.linalg.norm(t3_c),
+        ]
+    ).T
+
+    # We need to create vectors t1_v, t2_v, t3_v
+    q_lab_1 = q_lab(ei, ef, peak1.angles.two_theta)
+    q_lab_2 = q_lab(ei, ef, peak2.angles.two_theta)
+
+    # In identical fashion as described above Eq.79
+    r_mat_inv = np.linalg.inv(r_mat)
+    # TODO implement r_mat from goniometer module
+    t1_v = r_mat_inv @ q_lab_1
+    t3_v = np.cross(r_mat_inv @ q_lab_1, r_mat_inv @ q_lab_2)
+    t2_v = np.cross(t3_v, t1_v)
+    T_v = np.array(
+        [
+            t1_v / np.linalg.norm(t1_v),
+            t2_v / np.linalg.norm(t2_v),
+            t3_v / np.linalg.norm(t3_v),
+        ]
+    ).T
+
+    u_mat = T_v @ T_c.T
+    return u_mat
