321805c9618ad83d37133e557b9bfcabd819dac6
Michael Walsh <walshmm@ornl.gov>
2026-03-09 09:09:02 -0400

    add logging configuration to TAVI

--- src/tavi/__init__.py
+++ src/tavi/__init__.py
@@ -4,6 +4,11 @@ import neutrons_standard
 
 neutrons_standard.init("tavi")
 
+
+from tavi.meta.logging import init_logging  # noqa: E402
+
+init_logging()
+
 try:
     from ._version import __version__  # noqa: F401
 except ImportError:
