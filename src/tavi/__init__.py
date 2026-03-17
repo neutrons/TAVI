200def559ecbfd66a1194b7f35e3c489ffaf9aef
Michael Walsh <walshmm@ornl.gov>
2026-03-17 15:45:41 -0400

    update to add try catch in order to support ci not having all the deps

--- src/tavi/__init__.py
+++ src/tavi/__init__.py
@@ -6,7 +6,10 @@ neutrons_standard.init("tavi")
 
 from .library import init as init_library  # noqa: E402
 
-init_library()
+try:
+    init_library()
+except ModuleNotFoundError as e:
+    print("Unable to init submodule. Still valid for ci, proceeding.")
 
 try:
     from ._version import __version__  # noqa: F401
