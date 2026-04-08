c650cbcb101a7ef8ac2f12fa8785f49b32a6b3a3
Kyle Ma <kyleqianlima@gmail.com>
2026-04-08 12:17:35 -0400

    parse meta data

--- src/tavi/library/storage/loader/ornl_spice_loader.py
+++ src/tavi/library/storage/loader/ornl_spice_loader.py
@@ -33,9 +33,55 @@ class ORNLSpiceLoader(AbstractLoader):
         """Get score for scan."""
         return self.classifier.get_score(path, self.classification_rules)
 
-    def parse_metadata(self, path: str) -> ScanMetadata:
+    def parse_metadata(self, file_path: str) -> ScanMetadata:
         """Parse metadata."""
-        pass
+        with open(file_path, encoding="utf-8") as f:
+            all_content = f.readlines()
+        headers = [line.strip() for line in all_content if "#" in line]
+        index_col_name = headers.index("# col_headers =")
+        col_names = headers[index_col_name + 1].strip("#").split()
+        # remove the dot before it causes problem
+        # index_of_pt = col_names.index("Pt.")
+        # col_names[index_of_pt] = "Pt"
+        metadata_list = headers[:index_col_name]
+        error_messages = headers[index_col_name + 2 :]
+
+        index_sum_count = [i for i, header in enumerate(headers) if header.startswith("# Sum of Counts =")]
+        # in case "Sum of Counts" doesn't exist
+        # happens to the last scan after beam is down
+        if len(index_sum_count) != 0:
+            metadata_list += headers[index_sum_count[0] :]
+            error_messages = error_messages[: index_sum_count[0] - len(headers)]
+
+        metadata = {}
+        others = []
+
+        for metadata_entry in metadata_list:
+            line = metadata_entry.strip("# ")
+
+            if "completed" in line or "stopped" in line:  # last line
+                parts = line.split(" ")
+                end_time = parts[3] + " " + parts[0] + " " + parts[1]
+                metadata.update({"end_time": end_time})
+            # elif line[-1] == "=":  # empty line
+            #     unused.append(line[:-2])  # remove  " ="
+            elif "=" in line:  # useful line
+                parts = line.split("=")
+                key = parts[0].strip()
+                val = "=".join(parts[1:])[1:]  # remove the first space character
+                metadata.update({key: val})
+            else:  # how did you get here?
+                others.append(line)
+
+        if metadata.get("preset_type") == "countfile":  # HB1 in polarization mode
+            countfile = []
+        for metadata_entry in metadata_list:
+            if metadata_entry.startswith("# countfile"):
+                _, val = metadata_entry.split("=")
+                countfile.append(val.strip())
+        metadata.update({"countfile": ", ".join(countfile)})
+        data = metadata | {"errors": error_messages} | {"others": others}
+        return ScanMetadata(data)
 
     def parse_tavi_metadata(self, path: str) -> TaviMetadata:
         """Parse metadata."""
