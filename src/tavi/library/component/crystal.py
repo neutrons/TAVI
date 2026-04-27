9d253e1a6448a8c982a544c838109129e43397a0
Kyle Ma <kyleqianlima@gmail.com>
2026-04-27 14:23:58 -0400

    add crystal info for mono/ana

---
+++ src/tavi/library/component/crystal.py
@@ -0,0 +1,15 @@
+"""Contains d-spacing of monochromator and analyzer crystal, in angstrom."""
+crystal_d = {
+    "PG002": 3.35416,
+    "Pg002": 3.35416,
+    "PG004": 1.67708,
+    "Cu111": 2.08717,
+    "Cu220": 1.27813,
+    "Ge111": 3.26627,
+    "Ge220": 2.00018,
+    "Ge311": 1.70576,
+    "Ge331": 1.29789,
+    "Be002": 1.79160,
+    "Be110": 1.14280,
+    "Heusler": 3.435,  # Cu2MnAl(111)
+}
--- src/tavi/library/component/monochromater.py
+++ src/tavi/library/component/monochromater.py
@@ -1,10 +1,14 @@
 """Monochromater."""
 
 from pydantic import BaseModel
+from tavi.library.component.crystal import crystal_d
 
 
 class Monochromator(BaseModel):
     """Monochromater that holds horizontal and vertical mosaic."""
 
-    mosaic_h: float = 1.0
-    mosaic_v: float = 1.0
+    def __init__(self, mosaic_h:float = 30.0, mosaic_v:float = 30.0, crystal: str = "PG002"):
+        self.mosaic_h = mosaic_h
+        self.mosaic_v = mosaic_v
+        self.crystal = crystal
+        self.d_spacing = crystal_d.get(crystal, 0)
