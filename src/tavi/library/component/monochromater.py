bb178d0dc6c7525bb53ca85104c788c5b8b12e12
Kyle Ma <kyleqianlima@gmail.com>
2026-04-28 14:00:50 -0400

    fix pre-commit

--- src/tavi/library/component/monochromater.py
+++ src/tavi/library/component/monochromater.py
@@ -8,7 +8,8 @@ from tavi.library.component.crystal import crystal_d
 class Monochromator(BaseModel):
     """Monochromater that holds horizontal and vertical mosaic."""
 
-    def __init__(self, mosaic_h: float = 30.0, mosaic_v: float = 30.0, crystal: str = "PG002"):
+    def __init__(self, mosaic_h: float = 30.0, mosaic_v: float = 30.0, crystal: str = "PG002")->None:
+        """Init."""
         self.mosaic_h = mosaic_h
         self.mosaic_v = mosaic_v
         self.crystal = crystal
